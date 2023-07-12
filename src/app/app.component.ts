a11a4431d578dcf54cb417e9c5d583fe0bb68ebe
KindofHell <wildghost24@gmail.com>
2023-07-12 21:22:09 +0300

    Current currencies in Header + converter component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CurrencyService } from './currency.service';
+import { CurrencyConverterComponent } from './currency-converter/currency-converter.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'currency-app';
+export class AppComponent implements OnInit {
+  usdRate: number | undefined;
+  eurRate: number | undefined;
+
+  constructor(private currencyService: CurrencyService) {}
+
+  ngOnInit() {
+    this.currencyService.getCurrencyRates().subscribe((data: any) => {
+      const uahRate = data.rates.UAH;
+      this.usdRate = uahRate; // 1 USD is equivalent to UAH
+      this.eurRate = uahRate / data.rates.EUR; // Calculate how much UAH is equivalent to 1 EUR
+    });
+  }
 }
---
+++ src/app/currency-converter/currency-converter.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit } from '@angular/core';
+import { CurrencyService } from '../currency.service';
+
+@Component({
+  selector: 'app-currency-converter',
+  templateUrl: './currency-converter.component.html',
+  styleUrls: ['./currency-converter.component.css'],
+})
+export class CurrencyConverterComponent implements OnInit {
+  inputAmount: number | null = null;
+  inputCurrency: string = 'USD';
+  outputAmount: number | null = null;
+  outputCurrency: string = 'EUR';
+
+  exchangeRates: { [key: string]: number } = {};
+
+  constructor(private currencyService: CurrencyService) {}
+
+  ngOnInit() {
+    this.currencyService.getCurrencyRates().subscribe((data: any) => {
+      if (data && data.rates) {
+        this.exchangeRates = data.rates;
+        this.convertFromInput();
+      }
+    });
+  }
+
+  convertFromInput() {
+    if (this.inputAmount === null) {
+      this.outputAmount = null;
+      return;
+    }
+
+    this.outputAmount = this.convert(
+      this.inputAmount,
+      this.inputCurrency,
+      this.outputCurrency
+    );
+  }
+
+  convertFromOutput() {
+    if (this.outputAmount === null) {
+      this.inputAmount = null;
+      return;
+    }
+
+    this.inputAmount = this.convert(
+      this.outputAmount,
+      this.outputCurrency,
+      this.inputCurrency
+    );
+  }
+
+  convert(amount: number, fromCurrency: string, toCurrency: string): number {
+    if (fromCurrency === toCurrency) {
+      return amount;
+    }
+
+    const fromRate = this.exchangeRates[fromCurrency];
+    const toRate = this.exchangeRates[toCurrency];
+
+    if (fromRate && toRate) {
+      return (amount / fromRate) * toRate;
+    }
+
+    return amount;
+  }
+}
